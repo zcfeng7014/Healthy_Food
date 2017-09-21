617c9daa8db99b22fe41ba0bbf7bc0f188369459
zcfeng7014 <zcfeng7014@163.com>
2017-09-21 11:33:05 +0800

    init

---
+++ app/src/main/java/com/cfeng/study/healthy_food/bean/CMDBean.java
@@ -0,0 +1,32 @@
+package com.cfeng.study.healthy_food.bean;
+
+/**
+ * Created by Administrator on 2017/9/21 0021.
+ */
+
+public class CMDBean {
+
+    /**
+     * type : product
+     * value : 1
+     */
+
+    private String type;
+    private int value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
